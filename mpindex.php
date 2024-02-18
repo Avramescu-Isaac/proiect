85f679b128bf9f4e33b1ff1d2eadfd093eb8d6de
Avramescu-Isaac <156594958+Avramescu-Isaac@users.noreply.github.com>
2024-02-19 00:01:17 +0200

    Add files via upload

---
+++ mpindex.php
@@ -0,0 +1,423 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Revista &#8211; Online</title>
+    <link rel="stylesheet" href="mpindex.css">
+</head>
+
+<body>
+<div class="th">
+<div class="centernav bound">
+<div class="shme"><i class="fa fa-bars" aria-hidden="true"></i></div>
+<header role="banner">
+<span class="logos">
+<a title="Reviste - Online" itemprop="url" href="mpindex.php"><img src="iconnavbar.png"><span class="hdl">Reviste</span></a>
+</span>
+</header>
+<div class="searchx">
+<form action="" id="form" method="get" itemprop="potentialAction">
+<input id="s" itemprop="query-input" class="search-live" type="text" placeholder="Search" name="s" />
+</form>
+</div>
+</div>
+</div>
+<nav id="main-menu" class="mm">
+<div class="centernav">
+<div>
+<span>
+<ul><li><a href="/" aria-current="page" itemprop="url"><span itemprop="name">Home</span></a></li>
+<li><a href="bookmarks.php" itemprop="url"><span itemprop="name">Bookmarks</span></a></li>
+<li><a href="comics.php" itemprop="url"><span itemprop="name">Comics</span></a></li>
+</ul></span>
+<div class="clear"></div>
+</div>
+</div>
+</nav>
+<div id="content">
+<div class="wrapper">
+<div class="postbody">
+<div class="slidtop">
+<div class="trending">
+<div class="tdb">
+<div class="imgxa">
+<div class="imgxb" style="background-image: url('solomaxlevelnewbie.jpg');"></div>
+</div>
+</div>
+</div>
+</div>
+<div class="kln"><div id="pf-6021-1"></div></div><br/>
+<div class="bixbox hothome">
+<div class="releases">
+<h2>Popular Today</h2>
+</div>
+<div class="listupd">
+<div class="bs">
+<div class="bsx">
+<a href="manhwa1.html" title="Revenge of the Iron-Blooded Sword Hound">
+<div class="limit">
+<div class="ply"></div>
+<span class="type Manhwa">Manhwa</span> <img src="IronBloodSwordHound.png" /> </div>
+<div class="bigor">
+<div class="tt">
+Revenge of the Iron-Blooded Sword Hound </div>
+<div class="adds">
+<div class="epxs">Chapter 60</div>
+<div class="rt">
+<div class="rating">
+<div class="rating-prc">
+<div class="rtp">
+<div class="rtb"><span style="width:98%"></span></div>
+</div>
+</div>
+<div class="numscore">9.8</div>
+</div>
+</div>
+</div>
+</div>
+</a>
+</div>
+</div>
+<div class="bs">
+<div class="bsx">
+<a href="manhwa1.html" title="Revenge of the Iron-Blooded Sword Hound">
+<div class="limit">
+<div class="ply"></div>
+<span class="type Manhwa">Manhwa</span> <img src="IronBloodSwordHound.png" /> </div>
+<div class="bigor">
+<div class="tt">
+Revenge of the Iron-Blooded Sword Hound </div>
+<div class="adds">
+<div class="epxs">Chapter 60</div>
+<div class="rt">
+<div class="rating">
+<div class="rating-prc">
+<div class="rtp">
+<div class="rtb"><span style="width:98%"></span></div>
+</div>
+</div>
+<div class="numscore">9.8</div>
+</div>
+</div>
+</div>
+</div>
+</a>
+</div>
+</div>
+<div class="bs">
+<div class="bsx">
+<a href="manhwa1.html" title="Revenge of the Iron-Blooded Sword Hound">
+<div class="limit">
+<div class="ply"></div>
+<span class="type Manhwa">Manhwa</span> <img src="IronBloodSwordHound.png" /> </div>
+<div class="bigor">
+<div class="tt">
+Revenge of the Iron-Blooded Sword Hound </div>
+<div class="adds">
+<div class="epxs">Chapter 60</div>
+<div class="rt">
+<div class="rating">
+<div class="rating-prc">
+<div class="rtp">
+<div class="rtb"><span style="width:98%"></span></div>
+</div>
+</div>
+<div class="numscore">9.8</div>
+</div>
+</div>
+</div>
+</div>
+</a>
+</div>
+</div>
+<div class="bs">
+<div class="bsx">
+<a href="manhwa1.html" title="Revenge of the Iron-Blooded Sword Hound">
+<div class="limit">
+<div class="ply"></div>
+<span class="type Manhwa">Manhwa</span> <img src="IronBloodSwordHound.png" /> </div>
+<div class="bigor">
+<div class="tt">
+Revenge of the Iron-Blooded Sword Hound </div>
+<div class="adds">
+<div class="epxs">Chapter 60</div>
+<div class="rt">
+<div class="rating">
+<div class="rating-prc">
+<div class="rtp">
+<div class="rtb"><span style="width:98%"></span></div>
+</div>
+</div>
+<div class="numscore">9.8</div>
+</div>
+</div>
+</div>
+</div>
+</a>
+</div>
+</div>
+<div class="bs">
+<div class="bsx">
+<a href="manhwa1.html" title="Revenge of the Iron-Blooded Sword Hound">
+<div class="limit">
+<div class="ply"></div>
+<span class="type Manhwa">Manhwa</span> <img src="IronBloodSwordHound.png" /> </div>
+<div class="bigor">
+<div class="tt">
+Revenge of the Iron-Blooded Sword Hound </div>
+<div class="adds">
+<div class="epxs">Chapter 60</div>
+<div class="rt">
+<div class="rating">
+<div class="rating-prc">
+<div class="rtp">
+<div class="rtb"><span style="width:98%"></span></div>
+</div>
+</div>
+<div class="numscore">9.8</div>
+</div>
+</div>
+</div>
+</div>
+</a>
+</div>
+</div>
+<div class="bs">
+<div class="bsx">
+<a href="manhwa1.html" title="Revenge of the Iron-Blooded Sword Hound">
+<div class="limit">
+<div class="ply"></div>
+<span class="type Manhwa">Manhwa</span> <img src="IronBloodSwordHound.png" /> </div>
+<div class="bigor">
+<div class="tt">
+Revenge of the Iron-Blooded Sword Hound </div>
+<div class="adds">
+<div class="epxs">Chapter 60</div>
+<div class="rt">
+<div class="rating">
+<div class="rating-prc">
+<div class="rtp">
+<div class="rtb"><span style="width:98%"></span></div>
+</div>
+</div>
+<div class="numscore">9.8</div>
+</div>
+</div>
+</div>
+</div>
+</a>
+</div>
+</div>
+</div>
+</div>
+<div class="kln"><div id="pf-6011-1"></div></div><br/><div class="bixbox">
+<div class="releases"><h2>Latest Update</h2></div>
+<div class="listupd ">
+<div class="utao styletwo">
+<div class="uta">
+<div class="imgu">
+<a rel="225070" class="series" href=" manhwa2.php" title="The Player Hides His Past"><img src="thePlayerHideHisPast.png" class="ts-post-image wp-post-image attachment-post-thumbnail size-post-thumbnail" loading="lazy" /> </a>
+</div>
+<div class="luf">
+<a class="series" href=" manhwa2.php" title="The Player Hides His Past">
+<h4>The Player Hides His Past</h4>
+</a>
+<ul class="Manhwa"><li><a href="manhwa2-chapter3.php">Chapter 36</a><span>13 hours ago</span></li><li><a href="manhwa2-chapter2.php">Chapter 35</a><span>2 weeks ago</span></li><li><a href="manhwa2-chapter1.php">Chapter 34</a><span>2 weeks ago</span></li></ul> </div>
+</div>
+</div>
+<div class="utao styletwo">
+<div class="uta">
+<div class="imgu">
+<a rel="225070" class="series" href=" manhwa2.php" title="The Player Hides His Past"><img src="thePlayerHideHisPast.png" class="ts-post-image wp-post-image attachment-post-thumbnail size-post-thumbnail" loading="lazy" /> </a>
+</div>
+<div class="luf">
+<a class="series" href=" manhwa2.php" title="The Player Hides His Past">
+<h4>The Player Hides His Past</h4>
+</a>
+<ul class="Manhwa"><li><a href="manhwa2-chapter3.php">Chapter 36</a><span>13 hours ago</span></li><li><a href="manhwa2-chapter2.php">Chapter 35</a><span>2 weeks ago</span></li><li><a href="manhwa2-chapter1.php">Chapter 34</a><span>2 weeks ago</span></li></ul> </div>
+</div>
+</div>
+<div class="utao styletwo">
+<div class="uta">
+<div class="imgu">
+<a rel="225070" class="series" href=" manhwa2.php" title="The Player Hides His Past"><img src="thePlayerHideHisPast.png" class="ts-post-image wp-post-image attachment-post-thumbnail size-post-thumbnail" loading="lazy" /> </a>
+</div>
+<div class="luf">
+<a class="series" href=" manhwa2.php" title="The Player Hides His Past">
+<h4>The Player Hides His Past</h4>
+</a>
+<ul class="Manhwa"><li><a href="manhwa2-chapter3.php">Chapter 36</a><span>13 hours ago</span></li><li><a href="manhwa2-chapter2.php">Chapter 35</a><span>2 weeks ago</span></li><li><a href="manhwa2-chapter1.php">Chapter 34</a><span>2 weeks ago</span></li></ul> </div>
+</div>
+</div>
+<div class="utao styletwo">
+<div class="uta">
+<div class="imgu">
+<a rel="225070" class="series" href=" manhwa2.php" title="The Player Hides His Past"><img src="thePlayerHideHisPast.png" class="ts-post-image wp-post-image attachment-post-thumbnail size-post-thumbnail" loading="lazy" /> </a>
+</div>
+<div class="luf">
+<a class="series" href=" manhwa2.php" title="The Player Hides His Past">
+<h4>The Player Hides His Past</h4>
+</a>
+<ul class="Manhwa"><li><a href="manhwa2-chapter3.php">Chapter 36</a><span>13 hours ago</span></li><li><a href="manhwa2-chapter2.php">Chapter 35</a><span>2 weeks ago</span></li><li><a href="manhwa2-chapter1.php">Chapter 34</a><span>2 weeks ago</span></li></ul> </div>
+</div>
+</div>
+<div class="utao styletwo">
+<div class="uta">
+<div class="imgu">
+<a rel="225070" class="series" href=" manhwa2.php" title="The Player Hides His Past"><img src="thePlayerHideHisPast.png" class="ts-post-image wp-post-image attachment-post-thumbnail size-post-thumbnail" loading="lazy" /> </a>
+</div>
+<div class="luf">
+<a class="series" href=" manhwa2.php" title="The Player Hides His Past">
+<h4>The Player Hides His Past</h4>
+</a>
+<ul class="Manhwa"><li><a href="manhwa2-chapter3.php">Chapter 36</a><span>13 hours ago</span></li><li><a href="manhwa2-chapter2.php">Chapter 35</a><span>2 weeks ago</span></li><li><a href="manhwa2-chapter1.php">Chapter 34</a><span>2 weeks ago</span></li></ul> </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div id="sidebar">
+<div class="section"><div class="releases"><h3>Popular</h3></div>
+<div class="ts-wpop-series-gen">
+</div>
+<div id="wpop-items">
+<div class="serieslist pop wpop wpop-weekly">
+<ul>
+<li>
+<div class="ctr">1</div>
+<div class="imgseries">
+<a class="series" href="manhwa3.php" rel="275119">
+<img src="reincarnator.png" /> </a>
+</div>
+<div class="leftseries">
+<h2>
+<a class="series" href="manhwa3.php" rel="275119">Reincarnator</a>
+</h2>
+<span><b>Genres</b>: <a href="genres/action.php" rel="tag">Action</a>, <a href="genres/adventure.php" rel="tag">Adventure</a>, <a href="genres/fantasy.php" rel="tag">Fantasy</a>, <a href="genres/mystery.php" rel="tag">Mystery</a>, <a href="genres/reincarnation.php" rel="tag">Reincarnation</a>, <a href="genres/seinen.php" rel="tag">Seinen</a>, <a href="genres/supernatural.php" rel="tag">Supernatural</a></span> <div class="rt">
+<div class="rating">
+<div class="rating-prc">
+<div class="rtp">
+<div class="rtb"><span style="width:97%"></span></div>
+</div>
+</div>
+<div class="numscore">9.7</div>
+</div>
+</div>
+</div>
+</li>
+<li>
+	<div class="ctr">2</div>
+	<div class="imgseries">
+	<a class="series" href="manhwa3.php" rel="275119">
+	<img src="reincarnator.png" /> </a>
+	</div>
+	<div class="leftseries">
+	<h2>
+	<a class="series" href="manhwa3.php" rel="275119">Reincarnator</a>
+	</h2>
+	<span><b>Genres</b>: <a href="genres/action.php" rel="tag">Action</a>, <a href="genres/adventure.php" rel="tag">Adventure</a>, <a href="genres/fantasy.php" rel="tag">Fantasy</a>, <a href="genres/mystery.php" rel="tag">Mystery</a>, <a href="genres/reincarnation.php" rel="tag">Reincarnation</a>, <a href="genres/seinen.php" rel="tag">Seinen</a>, <a href="genres/supernatural.php" rel="tag">Supernatural</a></span> <div class="rt">
+	<div class="rating">
+	<div class="rating-prc">
+	<div class="rtp">
+	<div class="rtb"><span style="width:97%"></span></div>
+	</div>
+	</div>
+	<div class="numscore">9.7</div>
+	</div>
+	</div>
+	</div>
+	</li>
+	<li>
+		<div class="ctr">3</div>
+		<div class="imgseries">
+		<a class="series" href="manhwa3.php" rel="275119">
+		<img src="reincarnator.png" /> </a>
+		</div>
+		<div class="leftseries">
+		<h2>
+		<a class="series" href="manhwa3.php" rel="275119">Reincarnator</a>
+		</h2>
+		<span><b>Genres</b>: <a href="genres/action.php" rel="tag">Action</a>, <a href="genres/adventure.php" rel="tag">Adventure</a>, <a href="genres/fantasy.php" rel="tag">Fantasy</a>, <a href="genres/mystery.php" rel="tag">Mystery</a>, <a href="genres/reincarnation.php" rel="tag">Reincarnation</a>, <a href="genres/seinen.php" rel="tag">Seinen</a>, <a href="genres/supernatural.php" rel="tag">Supernatural</a></span> <div class="rt">
+		<div class="rating">
+		<div class="rating-prc">
+		<div class="rtp">
+		<div class="rtb"><span style="width:97%"></span></div>
+		</div>
+		</div>
+		<div class="numscore">9.7</div>
+		</div>
+		</div>
+		</div>
+		</li>
+		<li>
+			<div class="ctr">4</div>
+			<div class="imgseries">
+			<a class="series" href="manhwa3.php" rel="275119">
+			<img src="reincarnator.png" /> </a>
+			</div>
+			<div class="leftseries">
+			<h2>
+			<a class="series" href="manhwa3.php" rel="275119">Reincarnator</a>
+			</h2>
+			<span><b>Genres</b>: <a href="genres/action.php" rel="tag">Action</a>, <a href="genres/adventure.php" rel="tag">Adventure</a>, <a href="genres/fantasy.php" rel="tag">Fantasy</a>, <a href="genres/mystery.php" rel="tag">Mystery</a>, <a href="genres/reincarnation.php" rel="tag">Reincarnation</a>, <a href="genres/seinen.php" rel="tag">Seinen</a>, <a href="genres/supernatural.php" rel="tag">Supernatural</a></span> <div class="rt">
+			<div class="rating">
+			<div class="rating-prc">
+			<div class="rtp">
+			<div class="rtb"><span style="width:97%"></span></div>
+			</div>
+			</div>
+			<div class="numscore">9.7</div>
+			</div>
+			</div>
+			</div>
+		</li>
+		<li>
+			<div class="ctr">5</div>
+			<div class="imgseries">
+			<a class="series" href="manhwa3.php" rel="275119">
+			<img src="reincarnator.png" /> </a>
+			</div>
+			<div class="leftseries">
+			<h2>
+			<a class="series" href="manhwa3.php" rel="275119">Reincarnator</a>
+			</h2>
+			<span><b>Genres</b>: <a href="genres/action.php" rel="tag">Action</a>, <a href="genres/adventure.php" rel="tag">Adventure</a>, <a href="genres/fantasy.php" rel="tag">Fantasy</a>, <a href="genres/mystery.php" rel="tag">Mystery</a>, <a href="genres/reincarnation.php" rel="tag">Reincarnation</a>, <a href="genres/seinen.php" rel="tag">Seinen</a>, <a href="genres/supernatural.php" rel="tag">Supernatural</a></span> <div class="rt">
+			<div class="rating">
+			<div class="rating-prc">
+			<div class="rtp">
+			<div class="rtb"><span style="width:97%"></span></div>
+			</div>
+			</div>
+			<div class="numscore">9.7</div>
+			</div>
+			</div>
+			</div>
+			</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div id="footer">
+	<footer>
+		<div class="footermenu">
+				<ul id="menu-footer">
+					<li><a href="privacy-policy.php">Privacy Policy</a></li>
+					<li><a href="digital-millennium-copyright-act.php">Digital Millennium Copyright Act</a></li>
+					<li><a href="terms-of-service.php">Terms of Service</a></li>
+				</ul>
+			</div>
+			<div class="footercopyright">
+				<div class="copyright marx">
+					<div class="footer-logo"><img src="iconfooter.png"></div>
+					</div>
+				<p>For any questions or suggestions, you can contact us at the email address: contaczxcvbnmt@gmail.com</p>
+			</div>
+		</div>
+	</footer>
+</div>
+</body>
+</html>
